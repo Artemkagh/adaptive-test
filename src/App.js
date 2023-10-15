bf94b25bdfb67faa3257b69cb8cd0e2d8a8b6fbc
Artemkagh <gmcorp00x@mail.ru>
2023-10-16 00:55:19 +0300

    Добавил адекватный flex для карточек + стиль кнопки смены темы

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function App() {
-  const contentcol = document.querySelector(".contentcontainer");
   const [theme, setTheme] = React.useState(0);
-
+  const arr = new Array(10).fill("");
+  console.log(arr);
   const handleSwitchTheme = () => {
     setTheme(!theme);
     if(!theme){
@@ -13,15 +13,19 @@ function App() {
       document.body.style.backgroundColor = "rgb(209, 209, 209)";
     } 
   }
-
+  //<img src = {sunicon} className={`trigger${theme ? 'black' : ''}`}></img>
   return (
     <div className="maincontainer">
       <div className="headercontainer">
+        <p>Random text</p>
         <div onClick={handleSwitchTheme} className="switchtheme">
           <div className={`trigger${theme ? 'black' : ''}`}></div>
         </div>
       </div>
       <div className={`contentcontainer${theme ? 'black' : ''}`}>
+        {arr.map(() => {
+          return <div className={`element${theme ? 'black' : ''}`}></div>
+        })}
       </div>
       
     </div>
