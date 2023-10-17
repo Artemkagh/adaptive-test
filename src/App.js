33fa0acc9c69ab15362b18c509e7e0376c25ae3f
Artemkagh <gmcorp00x@mail.ru>
2023-10-18 00:37:31 +0300

    Сделал адаптивный дизайн

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import strawberry from './images/strawicon.svg'
-import icecream from './images/icecreamicon.svg'
+import homeicon from './images/homeicon.svg'
+import messageicon from './images/messageicon.svg'
+import plusicon from './images/plusicon.svg'
+import socialicon from './images/socialicon.svg'
+import quiticon from './images/quiticon.svg'
+
 
 function App() {
   const [theme, setTheme] = React.useState(0);
+  const [menu, setMenu] = React.useState(0);
   const arr = new Array(10).fill("");
   console.log(arr);
   const handleSwitchTheme = () => {
@@ -15,14 +21,19 @@ function App() {
       document.body.style.backgroundColor = "rgb(209, 209, 209)";
     } 
   }
-  //<img src = {sunicon} className={`trigger${theme ? 'black' : ''}`}></img>
+  const handleOpenMenu = () => {
+    setMenu(!menu);
+  }
   return (
     <div className="maincontainer">
       <div className="headercontainer">
-        <div className='sidemenucontainer'>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className={`${!menu ? 'sidemenucontainer' : 'sidemenucontaineractive'}`}>
+          <img src = {socialicon}></img>
+          <img src = {messageicon}></img>
+          <img src = {quiticon}></img>
+          <img src = {homeicon}></img>
+          <div onClick={handleOpenMenu} className={`${!menu ? 'plusdiv' : 'plusdivactive'}`}></div>
+          
         </div>
         <p>Random text</p>
         <div onClick={handleSwitchTheme} className="switchtheme">
